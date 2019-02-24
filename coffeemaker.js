ade8b4f6a12d60cb1f5443cca23709b3822ec441
Filipe de Andrade Morais <afilipem@gmail.com>
2019-02-23 18:09:27 -0600

    update: adding the last three functions for the cofeemaker, being the turnOffRequest, calls for the process and handleAppExit.

--- coffeemaker.js
+++ coffeemaker.js
@@ -11,3 +11,56 @@ client.on('connect', ()=>{
     client.publish('coffeemaker/connected', 'true')
     client.publish('coffeemaker/state', state)
 })
+
+client.on('message', (topic, message)=>{
+    console.log((""+message).red)
+    if (topic == 'controller'){
+        if(message=='turn on')
+            turnOnRequest()
+        else if(message=='turn off')
+            turnOffRequest()
+    }
+})
+
+function turnOnRequest(){
+    if (state=='on'||state=='starting up')return
+    else{
+        state='starting up'
+        console.log(('Starting up').yellow)
+        client.publish('coffeemaker/state', state)
+
+        setTimeout(()=>{
+            state = 'on'
+            console.log(('ON').yellow);
+            client.publish('coffeemaker/state', state)
+        }, 3000)
+    }
+}
+
+function turnOffRequest(){
+    if (state=='off'||state=='shutting-down')return
+    else{
+        state='shutting-down'
+        console.log(('Shutting Down').yellow);
+        client.publish('coffeemaker/state',state)
+
+        setTimeout(()=>{
+            state = 'off'
+            console.log(('OFF').yellow)
+            client.publish('coffeemaker/state',state)
+        },3000)
+    }
+}
+
+//Check for exceptions
+process.on('exit',handleAppExit.bind(null,{cleanup:true}))
+process.on('SIGINT',handleAppExit.bind(null, {exit:true}))
+process.on('uncaughtException', handleAppExit.bind(null,{exit:true}))
+
+//Notify controller
+function handleAppExit(options, err){
+    if (err){console.log(err.stack)}
+    if (options.cleanup)
+        {client.publish('coffeemaker/connected','false')}
+    if(options.exit){process.exit()}
+}
